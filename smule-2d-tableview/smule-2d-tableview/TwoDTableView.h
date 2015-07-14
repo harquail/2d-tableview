e83bbf4d7c13b75d463481111e99d5ec801b01ab
harquail <nook@harquail.com>
2015-07-14 02:19:47 -0400

    small progress

--- smule-2d-tableview/smule-2d-tableview/TwoDTableView.h
+++ smule-2d-tableview/smule-2d-tableview/TwoDTableView.h
@@ -10,53 +10,15 @@
 
 
 
-@protocol TwoDTableViewDataSource<NSObject>
+@protocol TwoDTableViewDataSource<UITableViewDataSource>
 
-@required
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
-
-// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-
-@optional
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;              // Default is 1 if not implemented
-
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section;    // fixed font style. use custom view (UILabel) if you want something different
-- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section;
-
-// Editing
-
-// Individual rows can opt out of having the -editing property set for them. If not implemented, all rows are assumed to be editable.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath;
-
-// Moving/reordering
-
-// Allows the reorder accessory view to optionally be shown for a particular row. By default, the reorder control will be shown only if the datasource implements -tableView:moveRowAtIndexPath:toIndexPath:
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath;
-
-// Index
-
-- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView;                                                    // return list of section titles to display in section index view (e.g. "ABCD...Z#")
-- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index;  // tell table which section corresponds to section title/index (e.g. "B",1))
-
-// Data manipulation - insert and delete support
-
-// After a row has the minus or plus button invoked (based on the UITableViewCellEditingStyle for the cell), the dataSource must commit the change
-// Not called for edit actions using UITableViewRowAction - the action's handler will be invoked instead
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath;
-
-// Data manipulation - reorder / moving support
-
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath;
 
 @end
 
+@protocol TwoDTableViewDelegate<UITableViewDelegate>
 
 
+@end
 
 @interface TwoDTableView : UITableView
 
---
+++ smule-2d-tableview/smule-2d-tableview/TwoDTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TwoDTableViewController.h
+//  smule-2d-tableview
+//
+//  Created by nook on 7/14/15.
+//  Copyright (c) 2015 nook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TwoDTableView.h"
+
+@interface TwoDTableViewController : UIViewController <TwoDTableViewDataSource, TwoDTableViewDelegate>
+
+@end
